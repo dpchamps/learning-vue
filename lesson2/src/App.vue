0f75deae85ea2783a82ae3a29cf1b77cc8333867
gasolinewaltz <gasolinewaltz@gmail.com>
2017-05-09 13:15:00 -0700

    The hot module demo. So effing cool!

---
+++ lesson2/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <counter></counter>
+  </div>
+</template>
+
+<script>
+  import counter from './components/counter.vue';
+  export default {
+    name: 'app',
+    components: {counter},
+    data () {
+      return {
+        msg: 'Welcome to Your Vue.js App'
+      }
+    }
+  }
+</script>
+
+<style>
+
+</style>
---
+++ lesson2/src/components/counter.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h2>The count: {{ count }}</h2>
+
+        <p>
+
+            <button @click="count += 10"> Increment </button>
+
+        </p>
+    </div>
+</template>
+
+<script type="text/babel">
+    export default {
+        data(){
+            return {
+                count : 0
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
